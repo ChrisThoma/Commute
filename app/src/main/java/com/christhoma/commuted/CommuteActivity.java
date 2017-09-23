f66d96b0512ea4576a723b0fbbaa440609bff19a
Chris Thoma <christhoma@Chriss-MacBook-Pro.local>
2017-09-23 01:44:28 -0700

    Initial commit.

---
+++ app/src/main/java/com/christhoma/commuted/CommuteActivity.java
@@ -0,0 +1,13 @@
+package com.christhoma.commuted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommuteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commute);
+    }
+}
