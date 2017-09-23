00732c2e00c6aa799913b2c22cb4efba68f90d59
Chris Thoma <christhoma@Chriss-MacBook-Pro.local>
2017-09-23 12:14:22 -0700

    Updates the gradle versions to use lambdas, adds other activity.

--- app/src/main/java/com/christhoma/commuted/CommuteActivity.java
+++ app/src/main/java/com/christhoma/commuted/CommuteActivity.java
@@ -1,7 +1,10 @@
 package com.christhoma.commuted;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CommuteActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class CommuteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commute);
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(v -> startActivity(new Intent(this, ProgressActivity.class)));
+
+        TextView etaText = findViewById(R.id.eta_text);
+        etaText.setText(getString(R.string.eta_string, 10));
     }
 }
