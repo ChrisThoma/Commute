e5c0a923a1afcb703c3ebcf77e214fac03588737
Chris Thoma <christhoma@Chriss-MacBook-Pro.local>
2017-09-25 14:11:25 -0700

    Adds images and flips between them when dragging seekbar.

--- app/src/main/java/com/christhoma/commuted/ProgressActivity.java
+++ app/src/main/java/com/christhoma/commuted/ProgressActivity.java
@@ -1,10 +1,17 @@
 package com.christhoma.commuted;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgressActivity extends AppCompatActivity {
 
@@ -12,6 +19,7 @@ public class ProgressActivity extends AppCompatActivity {
     ImageSwitcher imageSwitcher;
     SeekBar seekBar;
     int seekbarPosition = 0;
+    List<ImageView> images = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +27,16 @@ public class ProgressActivity extends AppCompatActivity {
         setContentView(R.layout.activity_progress);
 
         imageSwitcher = findViewById(R.id.image_switcher);
+        imageSwitcher.setFactory(() -> new ImageView(this));
         seekBar = findViewById(R.id.seek_bar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if (i != seekbarPosition + 1) {
+            public void onProgressChanged(SeekBar seekBar, int position, boolean b) {
+                if (position != seekbarPosition + 1) {
                     seekBar.setProgress(seekbarPosition);
                 } else {
-                    seekbarPosition = i;
+                    seekbarPosition = position;
+                    setImage(position);
                 }
             }
 
@@ -40,5 +50,40 @@ public class ProgressActivity extends AppCompatActivity {
 
             }
         });
+
+        setImage(0);
+    }
+
+    private void setImage(int position) {
+        switch (position) {
+            case 0:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_1));
+                break;
+            case 1:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_2));
+                break;
+            case 2:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_3));
+                break;
+            case 3:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_4));
+                break;
+            case 4:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_5));
+                break;
+            case 5:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_6));
+                break;
+            case 6:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_7));
+                break;
+            case 7:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_8));
+                break;
+            case 8:
+                imageSwitcher.setImageDrawable(getDrawable(R.drawable.img_9));
+                break;
+            default:
+        }
     }
 }
